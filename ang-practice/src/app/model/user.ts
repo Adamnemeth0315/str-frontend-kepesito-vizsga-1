eb7a725bfe2ceb9ed42025d8708312879f54995c
Adamnemeth0315 <adam.nemeth.0315@gmail.com>
2021-05-31 12:48:12 +0200

    Váz kész

---
+++ ang-practice/src/app/model/user.ts
@@ -0,0 +1,6 @@
+export class User {
+    id?: number;
+    image: string = '';
+    name: string = '';
+    commits: number = 0;
+}
---
+++ ang-practice/src/app/service/user.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  BaseURL: string = "https://api.github.com/repos/angular/angular/contributors?per_page=100"
+
+  constructor( private http: HttpClient) { }
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(this.BaseURL);
+  }
+}
